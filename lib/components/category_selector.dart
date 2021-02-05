52c81f968f47614feb4e457646ba55d1acd80366
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-02-05 23:03:51 +0700

    add statefulness

--- lib/components/category_selector.dart
+++ lib/components/category_selector.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
 
-class CategorySelector extends StatelessWidget {
+class CategorySelector extends StatefulWidget {
+  CategorySelector({Key key}) : super(key: key);
+
+  @override
+  _CategorySelectorState createState() => _CategorySelectorState();
+}
+
+class _CategorySelectorState extends State<CategorySelector> {
+  int selectedIndex = 0;
+  final List<String> categories = ["Message", "Online", "Groups", "Requests"];
+
   @override
   Widget build(BuildContext context) {
-    int selectedIndex = 0;
-    final List<String> categories = ["Message", "Online", "Groups", "Requests"];
     return Container(
         height: 90,
         color: Theme.of(context).primaryColor,
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              child: Text(
-                categories[index],
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    letterSpacing: 1.5),
+            return GestureDetector(
+              child: Padding(
+                child: Text(
+                  categories[index],
+                  style: TextStyle(
+                      color: index == selectedIndex
+                          ? Colors.white
+                          : Colors.white60,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      letterSpacing: 1.2),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              onTap: () {
+                print(index);
+                setState(() {
+                  selectedIndex = index;
+                });
+                print(selectedIndex);
+              },
             );
           },
           itemCount: categories.length,
